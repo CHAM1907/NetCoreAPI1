7537971b0c051e8c56ae3660d21f9daacb6bcb32
NGOTHINGOCCHAM <ngothaovy260506@gmail.com>
2026-03-24 01:01:44 +0700

    BaiThucHanhSo4

---
+++ Baithuchanhso3/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using BaiThucHanhSo3.Models;
+
+namespace BaiThucHanhSo3.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
